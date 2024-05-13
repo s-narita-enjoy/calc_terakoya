c05ecdc5acf829f4a4040b3ff0c4f41b82447365
NaritaSota <tindwcel.0917@gmail.com>
2024-05-13 21:07:23 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    num1: int
+    num2: int
+    operator: str
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/calc")
+async def calc(num1: int, num2: int, operator: str):
+    answer = 0
+    if operator == '+':
+        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
+    elif operator == '*':
+        answer = num1 * num2
+    elif operator == '/':
+        answer = num1 / num2
+    return{answer}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000, log_level="info")
