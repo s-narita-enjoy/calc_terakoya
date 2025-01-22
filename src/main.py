bbdc48ad629b886a601ab24aada6ba4fd8d5cd19
s_narita <tanoshimitai147@gmail.com>
2025-01-22 19:20:48 +0900

    20250122

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# 全オリジンを許可するCORS設定
+# ※開発都合上、全許可で設定しています。商用等では適切な設定にしてください。
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # すべてのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # 全てのHTTPメソッドを許可
+    allow_headers=["*"],  # 全てのヘッダーを許可
+)
 
 class Item(BaseModel):
     num1: object
